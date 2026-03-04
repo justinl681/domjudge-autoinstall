bd5044a8cb1d9f6d79b4546d15d8ecb8fa37d681
Justin <justin.lorenzz2233@gmail.com>
2026-03-03 20:36:46 -0500

    Fix docker compose command for judgehosts

--- init.sh
+++ init.sh
@@ -14,7 +14,6 @@ done
 
 curl -fsSL https://get.docker.com | sudo sh
 wget https://raw.githubusercontent.com/justinl681/domjudge-autoinstall/refs/heads/main/docker-compose.yml
-sudo docker network create domjudge
 sudo docker compose up -d mariadb domserver
 
 echo "Waiting for domserver to be reachable at localhost:80..."
