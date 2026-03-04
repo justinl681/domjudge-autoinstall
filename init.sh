fbc62e41ab78f7277ba01749750150f7d3648cbf
Justin <justin.lorenzz2233@gmail.com>
2026-03-03 20:26:55 -0500

    Update Docker commands to use 'docker compose'

--- init.sh
+++ init.sh
@@ -5,9 +5,9 @@ if [ "$(stat -fc %T /sys/fs/cgroup)" != "cgroup2fs" ]; then
 fi
 
 curl -fsSL https://get.docker.com | sudo sh
-sudo docker network create domjudge
 wget https://raw.githubusercontent.com/justinl681/domjudge-autoinstall/refs/heads/main/docker-compose.yml
-sudo docker up -d mariadb domserver
+sudo docker network create domjudge
+sudo docker compose up -d mariadb domserver
 
 echo "Waiting for domserver to be reachable at localhost:80..."
 until curl -fsSL -o /dev/null http://localhost:80; do
